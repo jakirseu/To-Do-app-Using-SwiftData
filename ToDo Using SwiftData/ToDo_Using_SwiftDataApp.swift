5c3a966c686bc134d1a22b497f9512b5b9503778
Jakir Hossain <jakirseu@gmail.com>
2023-09-20 19:25:40 +0600

    Initial Commit

---
+++ ToDo Using SwiftData/ToDo_Using_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_Using_SwiftDataApp.swift
+//  ToDo Using SwiftData
+//
+//  Created by Jakir Hossain on 20/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_Using_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
